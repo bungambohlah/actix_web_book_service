368af6ff2ea9f5432c4a42984e93c6192c47e6a6
Afif Abdillah Jusuf <bungambohlah@gmail.com>
2022-11-15 12:00:09 +0700

    fix: :bug: use non-addressed host instead of loopback address

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ async fn main() -> std::io::Result<()> {
             .service(index)
             .configure(services::config)
     })
-    .bind(("127.0.0.1", 3000))?
+    .bind(("0.0.0.0", 3000))?
     .run()
     .await
 }
